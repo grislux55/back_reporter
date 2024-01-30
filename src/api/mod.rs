13b124da7b13494710ffaf653f8212a1dfa34928
grislux55 <jebchou.hg@gmail.com>
2024-01-30 21:53:55 +0800

    fix worng header parsing implementation
    Signed-off-by: grislux55 <jebchou.hg@gmail.com>

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -4,6 +4,7 @@ use rocket::{
     Request,
 };
 use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter};
+use uuid::Uuid;
 
 use crate::orm::entities::{prelude::WeChatSession, we_chat_session};
 
@@ -11,7 +12,7 @@ pub mod user_info;
 pub mod wechat_login;
 
 pub struct BearerToken {
-    pub token: String,
+    pub token: Uuid,
 }
 
 #[rocket::async_trait]
@@ -20,9 +21,10 @@ impl<'r> FromRequest<'r> for BearerToken {
 
     async fn from_request(request: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
         match request.headers().get_one("Authorization") {
-            Some(header) if header.starts_with("Bearer ") => Outcome::Success(BearerToken {
-                token: header[7..].to_string(),
-            }),
+            Some(header) if header.starts_with("Bearer ") => match Uuid::parse_str(&header[7..]) {
+                Ok(token) => Outcome::Success(BearerToken { token }),
+                Err(_) => Outcome::Error((Status::Unauthorized, ())),
+            },
             _ => Outcome::Error((Status::Unauthorized, ())),
         }
     }
@@ -30,10 +32,10 @@ impl<'r> FromRequest<'r> for BearerToken {
 
 pub async fn validate_token(
     db: &DatabaseConnection,
-    token: &str,
+    token: &Uuid,
 ) -> Result<we_chat_session::Model, Status> {
     match WeChatSession::find()
-        .filter(we_chat_session::Column::LastToken.eq(token))
+        .filter(we_chat_session::Column::LastToken.eq(*token))
         .one(db)
         .await
     {
